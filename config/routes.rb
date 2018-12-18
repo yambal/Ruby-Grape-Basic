3d882eba477bfe84cd3e0d426dc10ab915daf54b
June YAMAMOTO <yamamoto@questetra.com>
2018-12-19 00:10:42 +0900

    Official Sample Twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Test::API => '/test/'
+  mount Twitter::API => '/twitter/'
+
+  # [path]/test/v2/api.rb のこと
+  # api.rb の 構成も Module、Class などで規制される
+  mount Test::V2::API => '/test/v2/'
 end
